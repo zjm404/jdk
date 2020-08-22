1fdf1f64e3dacc04515f2b6d106fb6bb32e1d7d8
随便 <42101994+zjm404@users.noreply.github.com>
2020-08-22 14:11:09 +0800

    init

---
+++ openjdk-14.0.1/src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "14.0.1+7" },
+            { "jdk", "14.0.1" },
+            { "release", "14.0.1" },
+        };
+    }
+}
